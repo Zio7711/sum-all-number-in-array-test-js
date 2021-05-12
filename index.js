287a5db45f7c01f8d51b6900f2569a4ca340216b
Zio <zi.ao.tan@outlook.com>
2021-05-12 04:55:35 +0000

    Add recursive function

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
+function sumItems(array, index = 0) {
+  if (index === array.length) {
+    return 0;
+  }
+
+  if (Array.isArray(array[index])) {
+    return sumItems(array[index], 0) + sumItems(array, index + 1);
+  }
+  return array[index] + sumItems(array, index + 1);
 }
 
 module.exports = sumItems;
